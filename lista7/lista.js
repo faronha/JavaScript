bc2bdba369914c0cccaba193b781e8f71ec1e9d4
faronha <gafasa400@gmail.com>
2025-02-11 10:15:46 -0300

    lista6

---
+++ lista7/lista.js
@@ -0,0 +1,27 @@
+
+let contador = 1
+
+while (contador <= 10){
+console.log(contador)
+contador += 1
+}
+
+console.log("----------------")
+
+let numero = prompt("Digite um número para ver sua tabuada:");
+let taboada = 1;
+
+while (taboada <= 10) {
+  let resultado = numero * taboada
+  console.log(resultado)
+  taboada++
+}
+
+console.log("---------------")
+
+let contagem = 10
+
+while (contagem <= 10){
+console.log(contagem)
+contador -=1
+}
